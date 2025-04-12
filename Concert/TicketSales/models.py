c0a2f98e51611dfcb55932df4711cc9c1c8eb905
mohammad-azimi <mohammadzimi.com@gmail.com>
2025-04-12 22:07:54 +0400

    feat: Configure media settings and update concert ticket sale components
    - Updated settings.py:
      - Set MEDIA_URL and MEDIA_ROOT for media file handling.
    - Modified urls.py:
      - Imported settings and static to enable media file serving in DEBUG mode.
      - Added URL pattern for ConcertListView at 'ticketsales/concert/list'.
    - Updated app.py:
      - Set verbose_name to "Selling concert tickets" for better admin display.
    - Refactored models.py:
      - Added/updated verbose_name for models and individual fields.
    - Implemented ConcertListView in views.py:
      - Created a basic view that lists ConcertModel objects in an HTML response.
    - Pre-populated sample data via superuser for testing models.
    
    This commit sets up media file configurations and lays the groundwork for the concert ticket sales functionality.

--- Concert/TicketSales/models.py
+++ Concert/TicketSales/models.py
@@ -1,63 +1,83 @@
 from django.db import models
 
 class ConcertModel(models.Model):
-    Name = models.CharField(max_length=100)
-    SingerName = models.CharField(max_length=100)
-    Lenght = models.IntegerField()
-    Poster = models.ImageField(upload_to="ConcertImages/",null=True)
+    class Meta:
+        verbose_name = "Concert"
+        verbose_name_plural = "Concerts"
+        
+    Name = models.CharField(max_length=100, verbose_name = "Title of the Concert")
+    SingerName = models.CharField(max_length=100, verbose_name = "Performer Name")
+    Lenght = models.IntegerField(verbose_name = "Concert Duration")
+    Poster = models.ImageField(upload_to="ConcertImages/",null=True, verbose_name = "Upload Concert Poster")
     
     def __str__(self):
         return self.SingerName
     
 class LocationModel(models.Model):
-    Name = models.CharField(max_length=100)
-    Address = models.CharField(max_length=500,default="Milad Tower - Tehran")
-    Phone = models.CharField(max_length=11,null=True)
-    Capacity = models.IntegerField()
+    class Meta:
+        verbose_name = "Location"
+        verbose_name_plural = "Locations"
+    
+    Name = models.CharField(max_length=100, verbose_name = "Venue Name")
+    Address = models.CharField(max_length=500,default="Milad Tower - Tehran", verbose_name = "Address of the Concert Venue")
+    Phone = models.CharField(max_length=11,null=True, verbose_name = "Phone Number")
+    Capacity = models.IntegerField(verbose_name = "Capacity of the Venue")
     
     def __str__(self):
         return self.Name
 
 class TimeModel(models.Model):
-    ConcertModel = models.ForeignKey("ConcertModel",on_delete=models.PROTECT)
-    LocationModel = models.ForeignKey("LocationModel",on_delete=models.PROTECT)
-    StartDateTime = models.DateTimeField()
-    Seats = models.IntegerField()
+    class Meta:
+        verbose_name = "Concert Session"
+        verbose_name_plural = "Concert Sessions"
+    
+    ConcertModel = models.ForeignKey("ConcertModel",on_delete=models.PROTECT, verbose_name= "Performer")
+    LocationModel = models.ForeignKey("LocationModel",on_delete=models.PROTECT, verbose_name= "Concert Venue")
+    StartDateTime = models.DateTimeField(verbose_name= "Concert Start Time")
+    Seats = models.IntegerField(verbose_name= "Number of Seats")
     Start = 1
     End = 2
     Cancel = 3
     Sales = 4
-    Status_Choises = (("Start","Ticket sales have started"),
-                      ("End","Ticket sales have ended"),
-                      ("Cancel","This concert session has been cancelled"),
-                      ("Sales","Selling tickets"))
+    Status_Choises = ((Start,"Ticket sales have started"),
+                      (End,"Ticket sales have ended"),
+                      (Cancel,"This concert session has been cancelled"),
+                      (Sales,"Selling tickets"))
     
-    Status = models.IntegerField(choices = Status_Choises)
+    Status = models.IntegerField(choices = Status_Choises, verbose_name= "Concert Session Status")
     
     def __str__(self):
-        return "Time: {} ConcertName: {} Location: {}".format(self.StartDateTime,ConcertModel.Name,LocationModel.Name)
+        return "Time: {}, ConcertName: {}, Location: {}".format(self.StartDateTime,self.ConcertModel.Name,self.LocationModel.Name)
     
 class ProfileModel(models.Model):
-    Name = models.CharField(max_length=100)
-    Family = models.CharField(max_length=100)
-    ProfileImage = models.ImageField(upload_to="ProfileImages/")
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+    
+    Name = models.CharField(max_length=100, verbose_name= "First Name")
+    Family = models.CharField(max_length=100, verbose_name= "Last Name")
+    ProfileImage = models.ImageField(upload_to="ProfileImages/", null=True, verbose_name= "Profile Picture")
     Man = 1
     Woman = 2
-    Status_Choises = (("Man","Man"),
-                      ("Woman","Woman"))
+    Status_Choises = ((Man,"Man"),
+                      (Woman,"Woman"))
     
-    Gender = models.IntegerField(choices = Status_Choises)
+    Gender = models.IntegerField(choices = Status_Choises, verbose_name= "Gender")
     
     def __str__(self):
-        return "FullName: {} {}".format(self.Name,self.Family)
+        return "{} {}".format(self.Name,self.Family)
     
 class TicketModel(models.Model):
-    ProfileModel = models.ForeignKey("ProfileModel",on_delete=models.PROTECT)
-    TimeModel = models.ForeignKey("TimeModel",on_delete=models.PROTECT)
-    Name = models.CharField(max_length=100)
-    Price = models.IntegerField()
-    TicketImage = models.ImageField(upload_to="TicketImages/")
+    class Meta:
+        verbose_name = "Ticket"
+        verbose_name_plural = "Tickets"
+        
+    ProfileModel = models.ForeignKey("ProfileModel",on_delete=models.PROTECT, verbose_name= "Full Name")
+    TimeModel = models.ForeignKey("TimeModel",on_delete=models.PROTECT, verbose_name= "Concert Session Info")
+    Name = models.CharField(max_length=100, verbose_name= "Type of Ticket")
+    Price = models.IntegerField(verbose_name= "Concert Ticket Price")
+    TicketImage = models.ImageField(upload_to="TicketImages/", verbose_name= "Concert Ticket Image")
     
     def __str__(self):
-        return "TicketInfo: Profile: {} ConcertInfo: {}".format(ProfileModel.__str__(),TimeModel.__str__())
+        return "Full Name: {}, ConcertInfo: {}".format(self.ProfileModel.__str__(),self.TimeModel.__str__())
     
--- Concert/TicketSales/views.py
+++ Concert/TicketSales/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from TicketSales.models import ConcertModel
 
-# Create your views here.
+def ConcertListView(request):
+    Concerts = ConcertModel.objects.all()
+    Text = """
+        <!DOCTYPE html>
+        <html>
+            <head></head>
+            <body>
+                <h1>List of available concerts</h1>
+                <ul>
+                    {}
+                </ul>
+            </body>
+        </html>
+    """.format('\n'.join('<li>{}</li>'.format(concert) for concert in Concerts))
+    return HttpResponse(Text)
