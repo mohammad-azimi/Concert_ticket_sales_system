426df524436a001a2389a99274b7f31e0aa4b2be
mohammad-azimi <mohammadzimi.com@gmail.com>
2025-04-20 00:46:28 +0400

    Set up static and template structure, updated concert list view
    - Added STATIC_ROOT to settings.py for static files configuration
    - Edited ConcertListView to pass concert list and count to the template
    - Created base static and templates directories
    - Added initial test CSS file (style.css)
    - Created concertlist.html template for displaying concerts

--- Concert/TicketSales/views.py
+++ Concert/TicketSales/views.py
@@ -1,19 +1,11 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from TicketSales.models import ConcertModel
 
 def ConcertListView(request):
+
     Concerts = ConcertModel.objects.all()
-    Text = """
-        <!DOCTYPE html>
-        <html>
-            <head></head>
-            <body>
-                <h1>List of available concerts</h1>
-                <ul>
-                    {}
-                </ul>
-            </body>
-        </html>
-    """.format('\n'.join('<li>{}</li>'.format(concert) for concert in Concerts))
-    return HttpResponse(Text)
+    context ={
+        "concertlist": Concerts,
+        "concertcount": Concerts.count(),
+    } 
+    return render(request,"TicketSales/concertlist.html",context)
